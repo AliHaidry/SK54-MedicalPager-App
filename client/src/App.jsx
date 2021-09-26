c0a4f4ffa6cdbcad371854be74b7ef154d1700af
AliHaidry <alihaidry11@gmail.com>
2021-09-26 12:28:56 -0400

    Code Updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,22 @@
-import React from 'react'
-import { StreamChat } from 'stream-chat';
-import { Chat} from 'stream-chat-react';
-import Cookie from 'universal-cookie';
-import { ChannelListContainer, ChannelContainer } from './components'; 
+import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookie from "universal-cookie";
+import { ChannelListContainer, ChannelContainer } from "./components";
+import "./App.css";
 
-const apiKey = 'kvkdjchdkwqu';
+const apiKey = "kvkdjchdkwqu";
 const client = StreamChat.getInstance(apiKey);
 
 export const App = () => {
-    return (
-        <div className="app_wrapper">
-            <Chat client={client} theme='team light'>
-                <ChannelListContainer>
-                    
-                </ChannelListContainer>
-                <ChannelContainer>
-                    
-                </ChannelContainer>
-            </Chat>
-        </div>
-    )
-}
+  return (
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
+    </div>
+  );
+};
 
-export default App; 
+export default App;
--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import { ChannelList, useChatContext } from "stream-chat-react";
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
+import Cookies from "universal-cookie";
+import HospitalIcon from "../assets/hospital.png";
+import LogoutIcon from "../assets/logout.png";
+
+const SideBar = ({ logout }) => (
+    <div className="channel-list__sidebar">
+        <div className="channel-list__sidebar__icon1">
+            <div className="icon1__inner">
+                <img src={HospitalIcon} alt="Hospital" width="30" />
+            </div>
+        </div>
+        <div className="channel-list__sidebar__icon2">
+            <div className="icon1__inner" onClick={logout}>
+                <img src={LogoutIcon} alt="Logout" width="30" />
+            </div>
+        </div>
+    </div>
+);
 
 const ChannelListContainer = () => {
-    return (
-        <div>
-            ChannelListContainer
-        </div>
-    )
-}
+  return (
+    <>
+        <SideBar />
+    </>
+  );
+};
 
-export default ChannelListContainer
+export default ChannelListContainer;
