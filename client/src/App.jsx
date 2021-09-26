ce16efc5ed031ee9e3f0f6c249b9e053a56ca01d
AliHaidry <alihaidry11@gmail.com>
2021-09-25 20:47:13 -0400

    Initial File and Directory structure

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StreamChat } from 'stream-chat';
+import { stream } from 'stream-chat-react';
+import Cookie from 'universal-cookie';
+
+
+const apiKey = 'kvkdjchdkwqu';
+
+export const App = () => {
+    return (
+        <div className="app_wrapper">
+            <Chat>
+                
+            </Chat>
+        </div>
+    )
+}
